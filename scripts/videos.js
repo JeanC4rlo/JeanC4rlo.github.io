2bb68d19d8ba75d0995e983576ef7291ed9b56e7
JeanC4rlo <jeancs.santos1@gmail.com>
2024-11-10 18:29:45 -0300

    Atualização nos arquivos do site

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,26 +1,40 @@
 const videoEl = document.querySelector("video");
+let indiceArtista = videoEl.dataset.artistId;
 
 const fontesVideo = [
-    "https://jeanc4rlo.github.io/videos/half-alive/songs.mp4",
-    "https://jeanc4rlo.github.io/videos/half-alive/sophies-house.webm",
-    "https://jeanc4rlo.github.io/videos/half-alive/dimyu.webm",
-    "https://jeanc4rlo.github.io/videos/half-alive/summerland.webm",
-    "https://jeanc4rlo.github.io/videos/half-alive/still-feel.mp4",
-    "https://jeanc4rlo.github.io/videos/half-alive/time2.webm",
-    "https://jeanc4rlo.github.io/videos/half-alive/nobody.mp4",
-    "https://jeanc4rlo.github.io/videos/half-alive/whats-wrong.webm",
-    "https://jeanc4rlo.github.io/videos/half-alive/automatic.mp4",
-    "https://jeanc4rlo.github.io/videos/half-alive/subliminal.webm"
+    [ // Half Alive
+        "https://jeanc4rlo.github.io/videos/half-alive/songs.mp4",
+        "https://jeanc4rlo.github.io/videos/half-alive/sophies-house.webm",
+        "https://jeanc4rlo.github.io/videos/half-alive/dimyu.webm",
+        "https://jeanc4rlo.github.io/videos/half-alive/summerland.webm",
+        "https://jeanc4rlo.github.io/videos/half-alive/still-feel.mp4",
+        "https://jeanc4rlo.github.io/videos/half-alive/time2.webm",
+        "https://jeanc4rlo.github.io/videos/half-alive/nobody.mp4",
+        "https://jeanc4rlo.github.io/videos/half-alive/whats-wrong.webm",
+        "https://jeanc4rlo.github.io/videos/half-alive/automatic.mp4",
+        "https://jeanc4rlo.github.io/videos/half-alive/subliminal.webm"
+    ],
+    [ // Will Joseph Cook
+        "https://jeanc4rlo.github.io/videos/will-joseph-cook/little-miss.mp4",
+        "https://jeanc4rlo.github.io/videos/will-joseph-cook/mf-bassline.mp4",
+        "https://jeanc4rlo.github.io/videos/will-joseph-cook/the-dragon.mp4",
+        "https://jeanc4rlo.github.io/videos/will-joseph-cook/kisses.mp4"
+    ],
+    [ // Laufey
+        "https://jeanc4rlo.github.io/videos/laufey/bewitched.mp4",
+        "https://jeanc4rlo.github.io/videos/laufey/california.mp4",
+        "https://jeanc4rlo.github.io/videos/laufey/from-the-start.mp4"
+    ]
 ];
 
 let videoContador = 0;
-let totalVideos = fontesVideo.length;
+let totalVideos = fontesVideo[indiceArtista].length;
 
 function rodarVideo() {
     videoEl.classList.remove("anim-saida");
     videoEl.classList.add("anim-entrada");
 
-    videoEl.setAttribute("src", fontesVideo[videoContador]);
+    videoEl.setAttribute("src", fontesVideo[indiceArtista][videoContador]);
     videoEl.load();
     videoEl.play();
 }
@@ -28,8 +42,8 @@ function rodarVideo() {
 videoEl.addEventListener('ended', delayVideo, false);
 
 function delayVideo() {
-    videoEl.classList.add("anim-saida");
     videoEl.classList.remove("anim-entrada");
+    videoEl.classList.add("anim-saida");
 
     setTimeout(() => {
         videoEl.setAttribute("src", "");
@@ -41,10 +55,10 @@ function proximoVideo() {
     videoContador++;
     if(videoContador == totalVideos) {
         videoContador = 0;
-        rodarVideo(videoContador);
+        rodarVideo();
     }
     else {
-        rodarVideo(videoContador);
+        rodarVideo();
     }
 }
 
